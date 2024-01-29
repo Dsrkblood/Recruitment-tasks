175b03afac32a694280a7cc2098f9541a59fa9b1
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-01-29 21:49:19 +0100

    Remove unnecessary comments and improve logic

--- MedianaCalculator/CalculatorMedian.js
+++ MedianaCalculator/CalculatorMedian.js
@@ -14,7 +14,7 @@ expenses = {
 			food: [20, 11.9, 30.2, 11.9],
 		},
 		"04": {
-			food: [10, 20, 21.5],
+			food: [10.2, 11.5, 2.5],
 			fuel: [],
 		},
 	},
@@ -23,12 +23,12 @@ expenses = {
 
 function get_median_of_first_week_expenses(expenses) {
 	let result = null;
+	let arrayMedian = [];
 	let counter = 0;
 
 	function calculateMedian(values) {
 		const sortedValues = values.slice().sort((a, b) => a - b);
 		const middleIndex = Math.floor(values.length / 2);
-
 		if (values.length % 2 === 1 && values.length > 0) {
 			return sortedValues[middleIndex];
 		} else if (values.length % 2 === 0 && values.length > 0) {
@@ -38,34 +38,33 @@ function get_median_of_first_week_expenses(expenses) {
 		return 0;
 	}
 
-	function medianCalculation(expensesFood, expensesFuel) {
-		const medianFood = calculateMedian(expensesFood);
-		const medianFuel = calculateMedian(expensesFuel);
-		const total = medianFood + medianFuel;
+	function medianCalculation(arrayMedian) {
+		const medianTotal = calculateMedian(arrayMedian);
+		const total = medianTotal;
 		counter += total;
 	}
 
 	for (const data in expenses) {
 		const firstDayOfMonth = new Date(`${data}-01`);
 		const dayOfWeek = firstDayOfMonth.getDay();
-		let arrayFood = [];
-		let arrayFuel = [];
+		const isSunday = 6 - (6 - dayOfWeek);
 
 		for (const day in expenses[data]) {
 			const checkedDay = new Date(`${data}-${day}`);
 			const monthDay = checkedDay.getDate();
 
-			if (monthDay < 7 && (dayOfWeek === 0 || monthDay < 7)) {
-				arrayFood.push(...expenses[data][day].food);
-				arrayFuel.push(...expenses[data][day].fuel);
+			if (isSunday === 0 && checkedDay.getDay() === 0 && monthDay < 7) {
+				arrayMedian.push(...expenses[data][day].food);
+				arrayMedian.push(...expenses[data][day].fuel);
+			} else if (isSunday > 0 && monthDay < 7) {
+				arrayMedian.push(...expenses[data][day].food);
+				arrayMedian.push(...expenses[data][day].fuel);
 			}
 		}
-
-		if (arrayFood.length !== 0 && arrayFuel.length !== 0) {
-			medianCalculation(arrayFood, arrayFuel);
-		}
 	}
+	medianCalculation(arrayMedian);
 	result = counter.toFixed(2);
 	return result;
 }
+
 console.log(get_median_of_first_week_expenses(expenses));
